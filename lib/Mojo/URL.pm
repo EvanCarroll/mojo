f4837dba226adb7e91511adfae2643494c7ade39
Evan Carroll <me@evancarroll.com>
2019-01-25 02:09:12 -0600

    Optimzied URL parse regex
    New regex is single pass! Previously regex was implemented from spec
    directly, now we use the non-capture regex abilities of perl. This
    optimizes out 4 captures. Presumably the spec has to support engines that
    don't have non-capture patterns

--- lib/Mojo/URL.pm
+++ lib/Mojo/URL.pm
@@ -58,15 +58,13 @@ sub parse {
   my ($self, $url) = @_;
 
   # Official regex from RFC 3986
-  $url =~ m!^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?!;
-  $self->scheme($2)                         if defined $2;
-  $self->path($5)                           if defined $5;
-  $self->query($7)                          if defined $7;
-  $self->fragment(_decode(url_unescape $9)) if defined $9;
-  if (defined(my $auth = $4)) {
-    $self->userinfo(_decode(url_unescape $1)) if $auth =~ s/^([^\@]+)\@//;
-    $self->host_port($auth);
-  }
+  $url =~ m!^(?:([^:/?#]+):)?(?://(?:([^@/]+?)\@)?([^/?#]*))?([^?#]*)(?:\?([^#]*))?(?:#(.*))?!;
+  $self->scheme($1)                         if defined $1;
+  $self->userinfo(_decode(url_unescape $2)) if defined $2;
+  $self->host_port($3);
+  $self->path($4)                           if defined $4;
+  $self->query($5)                          if defined $5;
+  $self->fragment(_decode(url_unescape $6)) if defined $6;
 
   return $self;
 }
